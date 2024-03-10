2e4a3daceab84bf9c40b2074133712e85bc77e79
rudendan <ruden.dan@gmail.com>
2024-03-10 20:13:46 +0200

    HW-2 was done

--- HW-2/src/main/java/org/example/Main.java
+++ HW-2/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+        ValueCalculator calculator = new ValueCalculator(1);
+        calculator.executeWithTwoThreads();
+        calculator.executeWithOneThread();
+        calculator.executeWithSpecificNumberOfThreads(5);
     }
 }
