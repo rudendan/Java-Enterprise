9e2e5128ba7599bfec85b101958f8ac887a93361
rudendan <ruden.dan@gmail.com>
2024-03-20 07:46:04 +0200

    Folder HomeWork-5 was created

---
+++ HomeWork-5/src/main/java/com/example/HomeWork5/HomeWork5Application.java
@@ -0,0 +1,13 @@
+package com.example.HomeWork5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork5Application.class, args);
+	}
+
+}
