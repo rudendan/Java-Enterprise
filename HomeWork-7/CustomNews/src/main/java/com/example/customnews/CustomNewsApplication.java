b49238a7d8a1d98372005334fe77ec2476b56dfa
rudendan <ruden.dan@gmail.com>
2024-03-24 15:59:22 +0200

    Folder HomeWork-7 with two apps was created

---
+++ HomeWork-7/CustomNews/src/main/java/com/example/customnews/CustomNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.customnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomNewsApplication.class, args);
+	}
+
+}
