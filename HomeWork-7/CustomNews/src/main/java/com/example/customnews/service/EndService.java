7c3243d3335bebf8c5d2f838baf529933c522dc0
rudendan <ruden.dan@gmail.com>
2024-03-27 15:46:47 +0200

    Unused annotations were deleted, url was moved to properties

--- HomeWork-7/CustomNews/src/main/java/com/example/customnews/service/EndService.java
+++ HomeWork-7/CustomNews/src/main/java/com/example/customnews/service/EndService.java
@@ -3,7 +3,6 @@ package com.example.customnews.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
